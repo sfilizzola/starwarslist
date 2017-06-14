61716deec6ddd870e30f36e6c6362d9b520a6426
Samuel Filizzola <sfilizzola@gmail.com>
2017-06-14 16:47:21 -0300

    Initial commit

---
+++ app/src/main/java/com/example/sfilizzola/starwarslist/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.sfilizzola.starwarslist;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by sfilizzola on 14/06/17.
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_layout);
+    }
+}
